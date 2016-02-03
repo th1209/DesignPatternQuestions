4c518673ce6e3d1f1fa232b9130f5215bfd9f62c
Toshiki Hata <t.hata@oratta.net>
2016-02-03 22:48:55 +0900

    2016/02/03

---
+++ AnswerPhp/Bridge/main.php
@@ -0,0 +1,85 @@
+<?php
+namespace MyDesignPattern\Practice01;
+
+//以下、メインルーチン
+
+class Display{
+	private $displayImpl;
+	
+	public function __construct(DisplayImpl $displayImpl){
+		$this->displayImpl = $displayImpl;
+	}
+	
+	public function open() {
+		$this->displayImpl->rawOpen();
+	}
+	
+	public function printStr(){
+		$this->displayImpl->rawPrintStr();
+	}
+	
+	public function close() {
+		$this->displayImpl->rawClose();
+	}
+	
+	public function display() {
+		$this->open();
+		$this->printStr();
+		$this->close();
+	}
+}
+
+class CountDisplay extends Display{
+	public function __construct(DisplayImpl $displayImpl){
+		parent::__construct($displayImpl);
+	}
+	
+	public function multiDisplay($count){
+		$this->open();
+		for($i = 0; $i < $count; $i++){
+			$this->printStr();
+		}
+		$this->close();
+	}
+}
+
+abstract class DisplayImpl{
+	abstract public function rawOpen();
+	
+	abstract public function rawPrintStr();
+	
+	abstract public function rawClose();
+}
+
+class StringDisplayImpl extends DisplayImpl{
+	private $string;
+	private $strWidth;
+	
+	public function __construct($string){
+		$this->string = $string;
+		$this->strWidth = strlen($string);//全角文字の場合、1文字2byteでカウントしたいので、strlenを使う。
+	}
+	
+	public function rawOpen(){
+		$this->printHeaderAndFooter();
+	}
+	
+	public function rawPrintStr(){
+		echo '|' . $this->string . '|' . PHP_EOL;
+	}
+	
+	public function rawClose(){
+		$this->printHeaderAndFooter();
+	}
+	
+	private function printHeaderAndFooter(){
+		echo '+';
+		for($i = 0; $i < $this->strWidth; $i++){
+			echo '-';
+		}
+		echo '+';
+		echo PHP_EOL;
+	}
+}
+
+
